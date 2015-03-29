eb7266df6111f13819ce73bc0854f8490aa33b43
jose <e.josem91@gmail.com>
2015-03-29 06:11:02 -0600

    avance

--- Cartelera/Portadas/views.py
+++ Cartelera/Portadas/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
+from .models import Portada
+
+def portada_view(request,titulo):
+	cartelera = get_object_or_404(Portada, titulo=titulo)
+	return render(request, 'portadas.html',)
+
 
 # Create your views here.
